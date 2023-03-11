db4608bc6794ff956f9e16862be2adb2b7b9ba8d
IK88 <126251770+sulivan-ok@users.noreply.github.com>
2023-03-11 19:47:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-hello
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<meta charset="utf-8" /> 
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
+<meta name="description" content="" /> 
+<meta name="author" content="" /> 
+<title>Bunga Hoki</title> 
+<link rel="icon" type="image/x-icon" href="//cdn.shopify.com/s/files/1/0271/5533/3229/files/madame_72193722-8302-4bda-89f2-9b6236a7b00b_32x32.jpg?v=1614299363"/> 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" /> 
+<link href="css/styles.css" rel="stylesheet" /> 
+</head> 
+<body style="background-color: #000;"> 
+<div class="container-fluid"> 
+<div class="row"> 
+<div class="w-100"> 
+<a href="https://bit.ly/3myorbL"> 
+<img class="body-image" src="https://i.ibb.co/ngn95ZB/adss-img-1.png" style="width: 100%; height: auto;" alt=""> 
+</a> 
+</div> 
+</div> 
+</div> 
+</body> 
+</html>
